b296fadf46061a6053d7b7db533d3423e46d1a19
Fcast10 <f.castelli@ohm.space>
2025-12-04 14:26:24 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import math
+
+app_web = FastAPI()
+
+class InputData(BaseModel):
+    dry_mass: float
+    delta_v: float
+    isp: float = 2300
+    rho_g: float = 1.2
+
+class OutputData(BaseModel):
+    propellant_mass: float
+    wet_mass: float
+    tank_vol: float
+
+@app_web.post("/calculate", response_model=OutputData)
+def calculate(data: InputData):
+    g0 = 9.80665
+
+    mass_ratio = math.exp(data.delta_v / (data.isp * g0))
+    prop_mass = data.dry_mass * (mass_ratio - 1)
+    wet_mass = data.dry_mass + prop_mass
+    vol = prop_mass / data.rho_g
+
+    return OutputData(
+        propellant_mass=prop_mass,
+        wet_mass=wet_mass,
+        tank_vol=vol
+    )
