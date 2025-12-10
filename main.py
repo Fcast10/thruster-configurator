0a4b4636061cb4b342d76f10cd65ca16344236b4
Fcast10 <f.castelli@ohm.space>
2025-12-10 15:12:00 +0100

    Modification of the cylinder model

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ import math
 app = FastAPI()
 
 app.add_middleware( 
-    CORSMiddleware, allow_origins=["*"],
-    allow_credentials=True, allow_methods=["*"],
+    CORSMiddleware, 
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
     allow_headers=["*"], 
 )
 
 
 class InputData(BaseModel):
-    dry_mass: float
-    delta_v: float
-    isp: float = 2300
-    rho_g: float = 1.2
+    dry_mass: float         # [kg]
+    delta_v: float          # [s]
 
 class OutputData(BaseModel):
-    propellant_mass: float
-    wet_mass: float
-    tank_vol: float
+    propellant_mass: float  # [kg]
+    wet_mass: float         # [kg]
+    height_cyl: float       # [mm]
 
 # Endpoint di test #
 
@@ -33,16 +33,34 @@ def root():
 
 @app.post("/calculate", response_model=OutputData)
 def calculate(data: InputData):
-    g0 = 9.80665
 
-    mass_ratio = math.exp(data.delta_v / (data.isp * g0))
-    prop_mass = data.dry_mass * (mass_ratio - 1)
-    wet_mass = data.dry_mass + prop_mass
-    vol = prop_mass / data.rho_g
+    # Fixed Data
+
+    g0 = 9.80665          # [m/s^2]
+    isp = 80              # [s]
+    rho_g = 1000          # [kg/m^3]
+    diameter = 94.2       # [mm]
+    radius = diameter/2   # [mm]
+    H_dome = 17           # [mm]
+
+
+    mass_ratio = math.exp(data.delta_v / (isp * g0))  # dimensionless
+    prop_mass = data.dry_mass * (mass_ratio - 1)      # [kg]
+    wet_mass = data.dry_mass + prop_mass              # [kg]
+    total_vol = prop_mass / rho_g                     # [m^3]
+    total_vol = total_vol*1e9                         # [mm^3]
+    cross_area = math.pi*(radius)**2                  # [mm^2]
+    dome_vol = 2/3*math.pi*(radius)**2*H_dome         # [mm^3]
+    cyl_vol = total_vol - 2*dome_vol                  # [mm^3]
+
+    if cyl_vol < 0:
+         H_cylinder = 0
+    else:
+        H_cylinder = cyl_vol/cross_area    # [mm]
 
     return OutputData(
         propellant_mass=prop_mass,
         wet_mass=wet_mass,
-        tank_vol=vol
+        height_cyl=H_cylinder
     )
 
